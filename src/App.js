4652be66d6c0ce5fe03c4f6746f28e8c18ec298f
João Lourenço <joaodslourenco@gmail.com>
2022-04-21 21:41:22 -0300

    initial config of react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import AllQuotes from './pages/AllQuotes'
+import NewQuote from './pages/NewQuote'
+import QuoteDetail from './pages/QuoteDetail'
+
 function App() {
-  return <div>alo</div>
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App
